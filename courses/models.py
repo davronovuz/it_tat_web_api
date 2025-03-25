2224c7d410ca2340fb8f78dfea0b88f3dd48622e
Asilbek1-star <jasurfayzullaev85@gmail.com>
2025-03-25 10:58:46 +0500

    Models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,62 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from utility.models import Utility
 
-# Create your models here.
+
+class Course(Utility):
+    title = models.CharField(max_length=255, null=False, blank=False, verbose_name="Kurs nomi")
+    description = models.TextField(null=True, blank=True, verbose_name="Kurs haqida malumot")
+    image = models.ImageField(upload_to='course_images/', null=True, blank=True, verbose_name="Kurs rasmi",
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    weekly_hours = models.IntegerField(null=False, blank=False, verbose_name="Haftalik dars davomiyligi(soat)")
+    duration_moths = models.IntegerField(null=False, blank=False, verbose_name="Kurs davomiyligi(oy)")
+    total_hours = models.IntegerField(null=False, blank=False, verbose_name="Umumiy dars davomiyligi(soat)")
+    price_per_moths = models.DecimalField(null=False, blank=False, verbose_name="Umumiy narxi(oylik)", max_digits=10, decimal_places=2)
+    discount = models.DecimalField(null=True, blank=True, verbose_name="Chegrima(%)", max_digits=5, decimal_places=2)
+    user_ai = models.BooleanField(null=True, blank=True, verbose_name="AI bilan ishlash", default=True)
+    start_hour = models.TimeField(null=True, blank=True, verbose_name="Boshlanish vaqti(soat)")
+
+    class Meta:
+        verbose_name = "Kurs"
+        verbose_name_plural = "Kurslar"
+
+    def __str__(self):
+        return self.title
+
+
+class Mentor(Utility):
+    first_name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Ism")
+    last_name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Familiya")
+    image = models.ImageField(upload_to='mentor_images/', null=True, blank=True, verbose_name="Mentor rasmi",
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    courses = models.ManyToManyField(Course, verbose_name="Kurslar", related_name="mentors")
+    description = models.TextField(null=True, blank=True, verbose_name="Mentor haqida malumot")
+    experience_years = models.IntegerField(null=False, blank=False, verbose_name="Tajribasi(yil)")
+    student_count = models.IntegerField(null=False, blank=False, verbose_name="O'quvchi soni")
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Mentor"
+        verbose_name_plural = "Mentorlar"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Portfolio(Utility):
+    name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Portfolio nomi")
+    image = models.ImageField(upload_to='portfolio_images/', null=True, blank=True, verbose_name="Portfolio rasmi",
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    description = models.TextField(null=True, blank=True, verbose_name="Portfolio haqida malumot")
+    mentor = models.ForeignKey(Mentor, on_delete=models.SET_NULL, null=True, blank=True, related_name="portfolios",
+                               verbose_name="Mentor")
+    url = models.URLField(null=True, blank=True, verbose_name="Portfolio sahifasi linki")
+
+    class Meta:
+        verbose_name = "Portfolio"
+        verbose_name_plural = "Portfolio"
+
+    def __str__(self):
+        return self.name
