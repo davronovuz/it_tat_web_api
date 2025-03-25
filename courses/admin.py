49a01f1687e1f42e50dcfb198c5b23ecdc0a2d24
JozilovaZ <jozilovazuxra8899@gmail.com>
2025-03-25 14:08:42 +0200

    model

--- courses/admin.py
+++ courses/admin.py
@@ -1,27 +1,26 @@
 from django.contrib import admin
-from .models import Course, Portfolio, Mentor
+from .models import Course, Mentor, Portfolio
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ['title', 'image', 'weekly_hours', 'duration_moths', 'total_hours', 'price_per_moths',
-                    'discount', 'user_ai', 'start_hour', 'description']  # 'description'ni list_display'ga qo'shdik
-    list_filter = ['user_ai', 'start_hour']
-    search_fields = ['title', 'description']
-    list_display_links = ['title', 'description']  # Endi 'description' bu yerda ishlaydi
-
-
-@admin.register(Portfolio)
-class PortfolioAdmin(admin.ModelAdmin):
-    list_display = ['name', 'image', 'description', 'mentor', 'url']
-    list_filter = ['mentor']
-    search_fields = ['name', 'description']
-    list_display_links = ['name', 'image', 'description']
+    list_display = ('title','duration_months', 'weekly_hours', 'duration_hours', 'start_date', 'price_per_month')
+    list_filter = ('duration_months', 'uses_ai')
+    search_fields = ('title', 'description')
+    ordering = ('title', 'description')
 
 
 @admin.register(Mentor)
 class MentorAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'image', 'experience_years', 'student_count', 'description']  # 'courses'ni bu yerda olib tashladik
-    list_filter = ['experience_years', 'student_count']
-    search_fields = ['first_name', 'last_name', 'description']
-    list_display_links = ['first_name', 'last_name', 'image', 'description']  # 'courses'ni olib tashladik, chunki bu Many-to-Many yoki FK
+    list_display = ('first_name', 'last_name', 'experience_years', 'students_count')
+    list_filter = ('experience_years', 'students_count')
+    search_fields = ('first_name', 'last_name')
+    ordering = ('first_name', 'last_name')
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mentor', 'url')
+    list_filter = ('mentor',)
+    search_fields = ('name',)
+    ordering = ('name',)
--- courses/models.py
+++ courses/models.py
@@ -4,36 +4,33 @@ from utility.models import Utility
 
 
 class Course(Utility):
-    title = models.CharField(max_length=255, null=False, blank=False, verbose_name="Kurs nomi")
-    description = models.TextField(null=True, blank=True, verbose_name="Kurs haqida malumot")
-    image = models.ImageField(upload_to='course_images/', null=True, blank=True, verbose_name="Kurs rasmi",
-                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
-    weekly_hours = models.IntegerField(null=False, blank=False, verbose_name="Haftalik dars davomiyligi(soat)")
-    duration_moths = models.IntegerField(null=False, blank=False, verbose_name="Kurs davomiyligi(oy)")
-    total_hours = models.IntegerField(null=False, blank=False, verbose_name="Umumiy dars davomiyligi(soat)")
-    price_per_moths = models.DecimalField(null=False, blank=False, verbose_name="Umumiy narxi(oylik)", max_digits=10,
-                                          decimal_places=2)
-    discount = models.DecimalField(null=True, blank=True, verbose_name="Chegrima(%)")
-    user_ai = models.BooleanField(null=True, blank=True, verbose_name="AI bilan ishlash", default=True)
-    start_hour = models.TimeField(null=True, blank=True, verbose_name="Boshlanish vaqti(soat)")
+    title = models.CharField(max_length=255,null=False,blank=False,verbose_name="Kurs nomi")
+    description = models.TextField(null=True,blank=True,verbose_name="Kurs haqida ma'lumot")
+    duration_months = models.IntegerField(null=False,blank=False,verbose_name="Kurs davomiyligi (oy)")
+    weekly_hours = models.IntegerField(null=False,blank=False,verbose_name="Xaftalik dars (soat)")
+    duration_hours = models.IntegerField(null=False,blank=False,verbose_name="Dars davomiyligi (soat)")
+    start_date = models.DateField(null=True,blank=True,verbose_name="Boshlanish sanasi")
+    image = models.ImageField(upload_to="courses",null=True,blank=True,verbose_name="Kurs rasmi",
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'jpeg', 'webp', 'svg'])])
+    price_per_month = models.DecimalField(null=False,blank=False,verbose_name="Kursning narxi (oylik)",max_digits=10,decimal_places=2)
+    discount = models.IntegerField(null=True,blank=True,verbose_name="Chegirma (%)")
+    uses_ai = models.BooleanField(null=True,blank=True,verbose_name="Kursda AI foydalaniladimi ?",default=True)
 
     class Meta:
         verbose_name = "Kurs"
         verbose_name_plural = "Kurslar"
-
     def __str__(self):
         return self.title
 
-
 class Mentor(Utility):
-    first_name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Ism")
-    last_name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Familiya")
-    image = models.ImageField(upload_to='mentor_images/', null=True, blank=True, verbose_name="Mentor rasmi",
-                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
-    courses = models.ManyToManyField(Course, verbose_name="Kurslar", related_name="mentors")
-    description = models.TextField(null=True, blank=True, verbose_name="Mentor haqida malumot")
-    experience_years = models.IntegerField(null=False, blank=False, verbose_name="Tajribasi(yil)")
-    student_count = models.IntegerField(null=False, blank=False, verbose_name="O'quvchi soni")
+    first_name = models.CharField(max_length=255,null=False,blank=False,verbose_name="Ismi")
+    last_name = models.CharField(max_length=255,null=False,blank=False,verbose_name="Familiyasi")
+    image = models.ImageField(upload_to="mentors",null=True,blank=True,verbose_name="Mentor rasmi",
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'jpeg', 'webp', 'svg'])])
+    description = models.TextField(null=True,blank=True,verbose_name="Mentor haqida ma'lumot")
+    courses = models.ManyToManyField(Course,verbose_name="Kurslar",related_name="mentors")
+    experience_years = models.IntegerField(null=False,blank=False,verbose_name="Tajriba (yil)")
+    students_count = models.IntegerField(null=False,blank=False,verbose_name="Mentorning mamnun o'quvchilar soni")
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -41,24 +38,105 @@ class Mentor(Utility):
     class Meta:
         verbose_name = "Mentor"
         verbose_name_plural = "Mentorlar"
-
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Portfolio(Utility):
     name = models.CharField(max_length=255,null=False,blank=False,verbose_name="Portfolio nomi")
-    image = models.ImageField(upload_to='portfolio_images/',null=True,blank=True,verbose_name="Portfolio rasmi",
-                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
-    description = models.TextField(null=True,blank=True,verbose_name="Portfolio haqida malumot")
-    mentor=models.ForeignKey(Mentor,on_delete=models.SET_NULL,null=True,blank=True,related_name="portfolios",verbose_name="Mentor")
-    url = models.URLField(null=True,blank=True,verbose_name="Portfolio sahifasi linki")
+    image = models.ImageField(upload_to="portfolios",null=True,blank=True,verbose_name="Portfolio rasmi",
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'jpeg', 'webp', 'svg'])])
+    description = models.TextField(null=True,blank=True,verbose_name="Portfolio haqida ma'lumot")
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name="portfolios")
+    url= models.URLField(null=True,blank=True,verbose_name="Portfolio link")
 
     class Meta:
-        verbose_name="Portfolio"
-        verbose_name_plural="Portfolio"
-
+        verbose_name = "Portfolio"
+        verbose_name_plural = "Portfolios"
     def __str__(self):
         return self.name
+
+
+#
+# class Technology(Utility):
+#     title=models.CharField(max_length=255,null=True,blank=True,verbose_name="title")
+#     image=models.ImageField(upload_to="Technoly/",null=True,blank=True)
+#     course=models.ForeignKey(Course,on_delete=models.CASCADE,null=True,blank=True)
+#
+#     class Meta:
+#         verbose_name="Technology"
+#         verbose_name_plural="Technologies"
+#
+#
+#
+#
+#
+#
+#
+# class Registration(Utility):
+#     name=models.CharField(max_length=255,null=False,blank=False,verbose_name="Ismn")
+#     phone_number=models.CharField(max_length=17, null=False,blank=False,unique=True)
+#     course=models.ForeignKey(Course,on_delete=models.CASCADE,null=False,blank=False)
+#
+#
+#     class Meta:
+#         verbose_name="Registration"
+#         verbose_name_plural="Registration"
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+    #
+    # class FeedbackMentor(Utility):
+    #     full_name=models.CharField(max_length=255,null=False,blank=False,verbose_name="Ism")
+    #     video=models.FileField(upload_to="feedback_video/",null=True,blank=True,verbose_name="VIdeolar")
+    #     course=models.ForeignKey(Course,on_delete=models.SET_NULL,null=True,blank=True,verbose_name="Kurs")
+    #     mentor=models.ForeignKey(Mentor,on_delete=models.SET_NULL,null=True,blank=True)
+    #
+    #     class Meta:
+    #         verbose_name="Feedback"
+    #         verbose_name_plural="Feedback"
+    #     def __str__(self):
+    #         return self.full_name
+    #
+    # class FAQ(Utility):
+    #     title=models.CharField(max_length=255,null=False,blank=False,verbose_name="title")
+    #     description=models.TextField(null=False,blank=False,verbose_name="Malumot")
+    #
+    #     class Meta:
+    #         verbose_name="FAQ"
+    #         verbose_name_plural="FAQ"
+    #     def __str__(self):
+    #         return self.title
+    #
+    # class ProgramRequest(Utility):
+    #     name=models.CharField(max_length=255,null=False,blank=False)
+    #     phone_number=models.CharField(max_length=17,null=False,blank=False,unique=True)
+    #
+    #
+    #     class Meta:
+    #         verbose_name="ProgramRequest"
+    #         verbose_name_plural = "ProgramRequest"
+    #
+    #     def __str__(self):
+    #         return self.name
+    #
+    #
+    # class Course_lesson_video(Utility):
+    #     video=models.FileField(upload_to="course_lesson/",null=True,blank=True,verbose_name="videolar")
+    #     course=models.ForeignKey(Course,on_delete=models.CASCADE,null=True,blank=True)
+    #
+    #
+    #     class Meta:
+    #         verbose_name="Course_lesson_video"
+    #         verbose_name_plural="Course_lesson_video"
+    #
+    #     def __str__(self):
+    #         return self.video
+
+
+
+
 
 
