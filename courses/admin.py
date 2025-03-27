d48a90ea2d90bdabad7ddc7f2026736aef468450
abdufattohfattoyev <abdufattohfattoyev0@gmail.com>
2025-03-27 06:57:04 +0500

    testlar va models ishlashi

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Courses,Mentors,Feedback_Mentor,Registration,Portfolio,FAQ,ProgramRequest,Technology
 
 # Register your models here.
+admin.site.register(Courses)
+admin.site.register(Mentors)
+admin.site.register(Feedback_Mentor)
+admin.site.register(Registration)
+admin.site.register(Portfolio)
+admin.site.register(FAQ)
+admin.site.register(ProgramRequest)
+admin.site.register(Technology)
