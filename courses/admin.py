6f3ae50d8a3d138e406303e78a0e67eac67e8fda
davronovuz <davronovtatu@gmail.com>
2025-03-27 09:45:21 +0500

    models success

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Mentor, Portfolio
+from .models import Course, Mentor, Portfolio, Technology, CourseLessonVideo, FeedbackMentor, Registration,FAQ,ProgramRequest
 
 
 @admin.register(Course)
@@ -24,3 +24,60 @@ class PortfolioAdmin(admin.ModelAdmin):
     list_filter = ('mentor',)
     search_fields = ('name',)
     ordering = ('name',)
+
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course')
+    list_filter = ('course',)
+    search_fields = ('title',)
+    ordering = ('title',)
+
+
+@admin.register(FeedbackMentor)
+class FeedbackMentorAdmin(admin.ModelAdmin):
+    list_display = ('mentor', 'full_name', 'video', 'feedback_text')
+    list_filter = ('mentor',)
+    search_fields = ('full_name',)
+    ordering = ('full_name',)
+
+
+
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'course')
+    list_filter = ('course',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(CourseLessonVideo)
+class CourseLessonVideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'video')
+    list_filter = ('course',)
+    search_fields = ('title',)
+    ordering = ('title',)
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer')
+    list_filter = ('question',)
+    search_fields = ('question',)
+    ordering = ('question',)
+
+
+
+@admin.register(ProgramRequest)
+class ProgramRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+
+
+
+
+
--- courses/models.py
+++ courses/models.py
@@ -3,11 +3,14 @@ from django.db import models
 from utility.models import Utility
 
 
+
+
+
 class Course(Utility):
     title = models.CharField(max_length=255,null=False,blank=False,verbose_name="Kurs nomi")
     description = models.TextField(null=True,blank=True,verbose_name="Kurs haqida ma'lumot")
     duration_months = models.IntegerField(null=False,blank=False,verbose_name="Kurs davomiyligi (oy)")
-    weekly_hours = models.IntegerField(null=False,blank=False,verbose_name="Xaftalik dars (soat)")
+    weekly_hours = models.IntegerField(null=False,blank=False,verbose_name="Xaftalik dars (kun)")
     duration_hours = models.IntegerField(null=False,blank=False,verbose_name="Dars davomiyligi (soat)")
     start_date = models.DateField(null=True,blank=True,verbose_name="Boshlanish sanasi")
     image = models.ImageField(upload_to="courses",null=True,blank=True,verbose_name="Kurs rasmi",
@@ -56,3 +59,94 @@ class Portfolio(Utility):
     def __str__(self):
         return self.name
 
+class CourseLessonVideo(Utility):
+    title= models.CharField(max_length=255,null=True,blank=True,verbose_name="Dars nomi")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True, related_name="lesson_videos",
+                               verbose_name="Kurs")
+    video = models.FileField(upload_to='lesson_videos/', null=True, blank=True, verbose_name="Video",
+                             validators=[FileExtensionValidator(allowed_extensions=['mp4', 'mkv', 'avi','mov', 'wmv', 'webm','flv'])])
+
+    class Meta:
+        verbose_name = "Video"
+        verbose_name_plural = "Video"
+
+
+    def __str__(self):
+        return f"{self.video}"
+
+
+class Technology(Utility):
+    title=models.CharField(max_length=255,null=True,blank=True,verbose_name="title")
+    image=models.ImageField(upload_to="Technoly/",null=True,blank=True)
+    course=models.ForeignKey(Course,on_delete=models.CASCADE,null=True,blank=True)
+
+    class Meta:
+        verbose_name="Technology"
+        verbose_name_plural="Technologies"
+
+
+    def __str__(self):
+        return self.title
+
+
+class FeedbackMentor(Utility):
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Mentor")
+    full_name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Foydalanuvchi ismi")
+    video = models.FileField(
+        upload_to="feedback_videos/",
+        null=True, blank=True,
+        verbose_name="Fikr-mulohaza videosi",
+        validators=[FileExtensionValidator(allowed_extensions=["mp4", "mov", "avi", "mkv"])]
+    )
+    feedback_text = models.TextField(null=True, blank=True, verbose_name="Izoh (matn shaklida)")
+
+    class Meta:
+        db_table = "feedback_mentor"
+        verbose_name = "Mentor haqida fikr"
+        verbose_name_plural = "Mentorlar haqida fikrlar"
+
+
+    def __str__(self):
+        mentor_name = self.mentor.get_full_name() if self.mentor else "No Mentor"
+        return f"{self.full_name} - {mentor_name}"
+
+
+class Registration(Utility):
+    name=models.CharField(max_length=255,null=False,blank=False,verbose_name="Ismn")
+    phone_number=models.CharField(max_length=17, null=False,blank=False,unique=True)
+    course=models.ForeignKey(Course,on_delete=models.CASCADE,null=False,blank=False)
+
+
+    class Meta:
+        verbose_name="Registration"
+        verbose_name_plural="Registration"
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+class FAQ(Utility):
+    question=models.CharField(max_length=255,null=False,blank=False,verbose_name="title")
+    answer=models.TextField(null=False,blank=False,verbose_name="Malumot")
+
+    class Meta:
+        verbose_name="FAQ"
+        verbose_name_plural="FAQ"
+    def __str__(self):
+        return f"{self.question} - {self.answer}"
+
+
+class ProgramRequest(Utility):
+    name=models.CharField(max_length=255,null=False,blank=False)
+    phone_number=models.CharField(max_length=17,null=False,blank=False,unique=True)
+
+
+    class Meta:
+        verbose_name="ProgramRequest"
+        verbose_name_plural = "ProgramRequest"
+
+    def __str__(self):
+        return self.name
